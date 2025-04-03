884588b1b670fa5d1e5b0c71f3e7b3ecababf1d0
garveyMui <m77561m@icloud.com>
2025-04-03 23:05:51 +0800

    image chat function done

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
+import "@/styles/tailwind.css";
 
 const preview: Preview = {
   parameters: {
@@ -6,6 +8,32 @@ const preview: Preview = {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+      },
+    },
+    viewport: {
+      viewports: {
+        ...MINIMAL_VIEWPORTS,
+        mobile1: {
+          name: "iPhone 16 Pro",
+          styles: {
+            width: "1206px",
+            height: "2622px",
+          },
+        },
+        tablet: {
+          name: "iPad",
+          styles: {
+            width: "2732px",
+            height: "2048px",
+          },
+        },
+        desktop: {
+          name: "Mac",
+          styles: {
+            width: "2560px",
+            height: "1664px",
+          },
+        },
       },
     },
   },
